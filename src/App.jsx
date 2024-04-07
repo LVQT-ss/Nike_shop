55ca40a47f9bd0b00ee8020b4aca85df2084ef8a
LVQT <levietquocthinh@gmail.com>
2024-04-08 02:32:05 +0700

    section for webpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+const App = () => (
+
+  <main className="relative">
+    Nav
+    {/* <Nav></Nav> */}
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      Hero
+    </section>
+
+    <section className="padding">
+      PopularProduct
+    </section>
+    <section className="padding">
+      SuperQuality
+    </section>
+    <section className="padding-x py-10 ">
+      Service
+    </section>
+    <section className="padding">
+      SpecialOffer
+    </section>
+    <section className="bg-pale-blue padding">
+      CustomerServices
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      Subcribe
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      Footer
+    </section>
+
+
+  </main>
+
+)
+export default App;
